09e9466de612390a4e3a057da65851d9d1c7d224
kaichooo <wasekaicho@gmail.com>
2022-09-19 15:09:13 +0900

    initial commit

---
+++ WinForm/MainWindow.h
@@ -0,0 +1,64 @@
+#pragma once
+
+namespace WinForm {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// MainWindow の概要
+	/// </summary>
+	public ref class MainWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ここにコンストラクター コードを追加します
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		~MainWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainWindow::typeid));
+			this->SuspendLayout();
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			resources->ApplyResources(this, L"$this");
+			this->Name = L"MainWindow";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
