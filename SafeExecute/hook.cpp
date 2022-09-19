285b8d458d1b1cd161d23d3cef0c8682c23e3415
MachineHunter <kazu3matsu@gmail.com>
2022-09-19 17:44:10 +0900

    implemented IAT hook of Ordinal WinAPI

--- SafeExecute/hook.cpp
+++ SafeExecute/hook.cpp
@@ -7,8 +7,10 @@
 // 1: WindowsAPIの関数型の定義
 typedef BOOL(WINAPI* SETFILEATTRIBUTESA)(LPCSTR lpFileName, DWORD dwFileAttributes);
 typedef BOOL(WINAPI* ISDEBUGGERPRESENT)();
+typedef int(WINAPI* WSASTARTUP)(WORD wVersionRequired, LPWSADATA lpWSAData);
 SETFILEATTRIBUTESA orig_SetFileAttributesA;
 ISDEBUGGERPRESENT orig_IsDebuggerPresent;
+WSASTARTUP orig_WSAStartup;
 
 
 // 2: フック関数の用意（ココに悪性処理検出のロジックを書く）
@@ -35,22 +37,36 @@ bool WINAPI IsDebuggerPresent_Hook() {
     return orig_IsDebuggerPresent();
 }
 
+int WINAPI WSAStartup_Hook(
+    WORD wVersionRequired,
+    LPWSADATA lpWSAData
+) {
+    PreHook("WSAStartup");
+    MessageBoxA(NULL, "Hooked WSAStartup", "debug", MB_OK);
+    ExitProcess(1);
+    return orig_WSAStartup(wVersionRequired, lpWSAData);
+}
+
 
 // 3: フックする全てのWindowsAPIのリスト
 HookList hooklist = {
         HookFunc("kernel32.dll", "SetFileAttributesA", (void**)&orig_SetFileAttributesA, (void*)SetFileAttributesA_Hook),
-        HookFunc("kernel32.dll", "IsDebuggerPresent", (void**)&orig_IsDebuggerPresent, (void*)IsDebuggerPresent_Hook)
+        HookFunc("kernel32.dll", "IsDebuggerPresent", (void**)&orig_IsDebuggerPresent, (void*)IsDebuggerPresent_Hook),
+        HookFunc("ws2_32.dll", WSAStartup_Ordinal, (void**)&orig_WSAStartup, (void*)WSAStartup_Hook)
 };
 
 // ================================== ここまでを編集してください！ =====================================================
-
 
 void Hook() {
     for (HookFunc hookfunc : hooklist) {
         ULONG cbSize = 0;
         HANDLE hModule = GetModuleHandleA(0);
 
-        *hookfunc.origfunc = (void*)GetProcAddress(GetModuleHandleA(hookfunc.dllname), hookfunc.funcname);
+        if (hookfunc.isOrdinal)
+            *hookfunc.origfunc = (void*)GetProcAddress(GetModuleHandleA(hookfunc.dllname), MAKEINTRESOURCEA(hookfunc.func.ordinal));
+        else
+            *hookfunc.origfunc = (void*)GetProcAddress(GetModuleHandleA(hookfunc.dllname), hookfunc.func.name);
+
         PIMAGE_IMPORT_DESCRIPTOR pImageImportDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)ImageDirectoryEntryToData(hModule, TRUE, IMAGE_DIRECTORY_ENTRY_IMPORT, &cbSize);
 
         for (; pImageImportDescriptor->Name; pImageImportDescriptor++) {
@@ -67,7 +83,7 @@ void Hook() {
                 if (pfnImportedFunc == (FARPROC)*hookfunc.origfunc) {
                     MEMORY_BASIC_INFORMATION mbi;
                     DWORD dwJunk = 0;
-
+                    
                     VirtualQuery(pFirstThunk, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
                     VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &mbi.Protect);
                     pFirstThunk->u1.Function = (ULONGLONG)(DWORD_PTR)hookfunc.hook;
