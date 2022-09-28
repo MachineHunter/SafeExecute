3f8ee41a4f0832bb9177ce3976f87fe2551bb5fa
KOHsEMP <kohsuke0322@softbank.ne.jp>
2022-09-29 08:28:17 +0900

    testfile & change LF

---
+++ TestExecutables/ChangeDeskWallPaperW.cpp
@@ -0,0 +1,18 @@
+// デスクトップの壁紙を変更するテストコード
+#include <Windows.h>
+#include <string>
+
+
+int main(void){
+
+    bool ret;
+    char* file_name = "blackwallpaper.PNG";
+
+    ret = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, file_name,SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
+    if(ret){
+        MessageBoxA(NULL, file_name, "Changed desktop wall paper", MB_OK);
+    }else{
+        MessageBoxA(NULL, file_name, "Failed to change desktop wall paper", MB_OK);
+    }
+}
